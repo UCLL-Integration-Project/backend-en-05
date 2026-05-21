0c5faf3b4e640256bcfab69ec0243ed5ada743df
cerenyelbogaucll <r0989914@ucll.be>
2026-05-21 10:15:05 +0200

    deduplication

--- src/main/java/be/ucll/it/courses/backend/repository/EventRepository.java
+++ src/main/java/be/ucll/it/courses/backend/repository/EventRepository.java
@@ -5,10 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
+import java.time.OffsetDateTime;
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface EventRepository extends JpaRepository<Event, UUID>, JpaSpecificationExecutor<Event> {
     Optional<Event> findFirstByOrderByTimestampDesc();
+    Optional<Event> findFirstByDeviceIdAndTimestampBetween(String deviceId, OffsetDateTime from, OffsetDateTime to);
 }
