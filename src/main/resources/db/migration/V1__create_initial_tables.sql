0875197ad3a6b7bcb3c3f89260de231529b02bc9
Matteo <matteo.gradinaru@student.ucll.be>
2026-05-21 17:04:59 +0200

    fix: Cleaned V1

--- src/main/resources/db/migration/V1__create_initial_tables.sql
+++ src/main/resources/db/migration/V1__create_initial_tables.sql
@@ -2,9 +2,7 @@ CREATE TABLE IF NOT EXISTS users (
     id         BIGSERIAL    PRIMARY KEY,
     username   VARCHAR(255) UNIQUE,
     first_name VARCHAR(255),
-    last_name  VARCHAR(255),
-    token      VARCHAR(255),
-    password   VARCHAR(255)
+    last_name  VARCHAR(255)
 );
 
 CREATE TABLE IF NOT EXISTS devices (
